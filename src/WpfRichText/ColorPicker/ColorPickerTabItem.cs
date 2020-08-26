873be07fbf47958d14db981c021e41a4ab4ab608
Aizazahmed Shaikh <SHAIKH.AIZAZAHMED@HOTMAIL.CO.UK>
2020-08-26 17:04:08 +0100

    mAKING CORE

---
+++ src/WpfRichText/ColorPicker/ColorPickerTabItem.cs
@@ -0,0 +1,32 @@
+﻿ 
+
+using System.Windows.Controls;
+using System.Windows.Input;
+
+ namespace WpfRichText.ColorPicker
+{
+  public class ColorPickerTabItem : TabItem
+  {
+    protected override void OnMouseLeftButtonDown( MouseButtonEventArgs e )
+    {
+      if( e.Source == this || !this.IsSelected )
+      {
+        e.Handled = true;
+        return;
+      }
+
+      base.OnMouseLeftButtonDown( e );
+    }
+
+    protected override void OnMouseLeftButtonUp( MouseButtonEventArgs e )
+    {
+      //Selection on Mouse Up
+      if( e.Source == this || !this.IsSelected )
+      {
+        base.OnMouseLeftButtonDown( e );
+      }
+
+      base.OnMouseLeftButtonUp( e );
+    }
+  }
+}
---
+++ src/WpfRichText/ColorPicker/HsvColor.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace WpfRichText.ColorPicker
+{
+  internal struct HsvColor
+  {
+    public double H;
+    public double S;
+    public double V;
+
+    public HsvColor( double h, double s, double v )
+    {
+      H = h;
+      S = s;
+      V = v;
+    }
+  }
+}
